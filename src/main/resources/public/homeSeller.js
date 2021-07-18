ad4ec5572fb7aec341a9c0eb350176b4e3fe6238
MakeTheCut <foojunpinf@gmail.com>
2021-07-17 19:05:10 -0700

    homeSeller redirect fixed, homeSeller page can add items now but not tied to any seller ID

--- src/main/resources/public/homeSeller.js
+++ src/main/resources/public/homeSeller.js
@@ -1,12 +1,12 @@
 function nameUpdate(){
     var x = document.getElementById("gettingName");
-    document.getElementById("titleplaceholder").innerHTML = x.value;
+    document.getElementById("titleplaceholder").innerHTML = "Name:" + x.value;
 }
 function priceUpdate(){
     var x = document.getElementById("gettingPrice");
-    document.getElementById("priceplaceholder").innerHTML = x.value;
+    document.getElementById("priceplaceholder").innerHTML = "Price:" + x.value;
 }
 function descriptionUpdate(){
     var x = document.getElementById("gettingDescription");
-    document.getElementById("descriptionplaceholder").innerHTML = x.value;
+    document.getElementById("descriptionplaceholder").innerHTML = "Description:" + x.value;
 }
